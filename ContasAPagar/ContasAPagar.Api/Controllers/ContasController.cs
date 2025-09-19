e6bcaa6489be357f1ab1de903f13dbe70ab6cdd5
Victor Soares <victorsoaresds.contato@gmail.com>
2025-09-19 01:03:57 -0300

    Adicionando controllers com metodos de get e post para a criação e consulta das contas

---
+++ ContasAPagar/ContasAPagar.Api/Controllers/ContasController.cs
@@ -0,0 +1,72 @@
+﻿using ContasAPagar.Application.Interfaces;
+using ContasAPagar.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class ContasController : Controller
+{
+    private readonly IContaAPagarService _service;
+
+    public ContasController(IContaAPagarService service)
+    {
+        _service = service ?? throw new ArgumentNullException(nameof(service));
+    }
+
+    [HttpPost("/conta/adicionar")]
+    public async Task<IActionResult> AddConta([FromBody] ContaEntities conta)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        try
+        {
+            var result = await _service.AdicionarContaAsync(conta);
+            return CreatedAtAction(nameof(AddConta), result);
+        }
+        catch (DbUpdateException dbEx)
+        {
+            return BadRequest(new
+            {
+                Mensagem = "Erro ao salvar a conta no banco de dados.",
+                Detalhes = dbEx.InnerException?.Message ?? dbEx.Message
+            });
+        }
+        catch (ArgumentException argEx)
+        {
+            return BadRequest(new { Mensagem = argEx.Message });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                Mensagem = "Ocorreu um erro interno no servidor.",
+                Detalhes = ex.Message
+            });
+        }
+    }
+
+    [HttpGet("/conta/consultar")]
+    public async Task<IActionResult> Listar()
+    {
+        try
+        {
+            var contas = await _service.ListarContasAsync();
+            return Ok(contas);
+        }
+        catch (DbUpdateException dbEx)
+        {
+            return BadRequest(new
+            {
+                Mensagem = "Erro ao consultar as contas no banco de dados.",
+                Detalhes = dbEx.InnerException?.Message ?? dbEx.Message
+            });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new
+            {
+                Mensagem = "Ocorreu um erro interno no servidor.",
+                Detalhes = ex.Message
+            });
+        }
+    }
+}
--- ContasAPagar/ContasAPagar.Api/Program.cs
+++ ContasAPagar/ContasAPagar.Api/Program.cs
@@ -1,3 +1,10 @@
+using ContasAPagar.Application.Interfaces;
+using ContasAPagar.Application.Services;
+using ContasAPagar.Infrastructure.Data;
+using ContasAPagar.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DBConnectionContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped<IContaARepository, ContaRepository>();
+builder.Services.AddScoped<IContaAPagarService, ContaAPagarService>();
 
 var app = builder.Build();
 
