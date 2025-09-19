4924d1fb28edb474e10664269d64ae9e165e1978
Victor Soares <victorsoaresds.contato@gmail.com>
2025-09-19 01:01:03 -0300

    Criando entidade da conta

---
+++ ContasAPagar/ContasAPagar.Domain/Entities/ContaEntities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContasAPagar.Domain.Entities
+{
+    [Table("Contas")]
+    public class ContaEntities
+    {
+        public ContaEntities( string nome, decimal valorOriginal, DateTime dataVencimento, DateTime dataPagamento, decimal valorCorrigido, int diasAtraso)
+        {
+            Nome = nome;
+            ValorOriginal = valorOriginal;
+            DataVencimento = dataVencimento;
+            DataPagamento = dataPagamento;
+            ValorCorrigido = valorCorrigido;
+            DiasAtraso = diasAtraso;
+        }
+
+        [Key]
+        [Column("id_conta")]
+        public int Id { get; private set; }
+
+        [Column("nome_conta")]
+        public string Nome { get; private set; }
+
+        [Column("Valor_original")]
+        public decimal ValorOriginal { get; private set; }
+
+        [Column("Data_vencimento")]
+        public DateTime DataVencimento { get; private set; }
+
+        [Column("Data_pagamento")]
+        public DateTime DataPagamento { get; private set; }
+
+        [Column("Valor_corrigido")]
+        public decimal ValorCorrigido { get;  set; }
+
+        [Column("Dias_atraso")]
+        public int DiasAtraso { get;  set; }
+    }
+}
