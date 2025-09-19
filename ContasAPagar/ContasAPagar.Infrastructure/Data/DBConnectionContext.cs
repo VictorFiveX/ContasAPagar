22163ab6b6f3fca156a75fd12b6b860a0a6d2360
Victor Soares <victorsoaresds.contato@gmail.com>
2025-09-19 01:00:43 -0300

    Criando conexão e repository conectando com o banco de dados

---
+++ ContasAPagar/ContasAPagar.Infrastructure/Data/DBConnectionContext.cs
@@ -0,0 +1,19 @@
+﻿using ContasAPagar.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContasAPagar.Infrastructure.Data
+{
+    public class DBConnectionContext : DbContext
+    {
+    public DBConnectionContext(DbContextOptions<DBConnectionContext> options)
+    : base(options) { }
+
+
+        public DbSet<ContaEntities> Contas { get; set; }
+    }
+}
---
+++ ContasAPagar/ContasAPagar.Infrastructure/Repositories/ContaRepository.cs
@@ -0,0 +1,34 @@
+﻿using ContasAPagar.Application.Interfaces;
+using ContasAPagar.Domain.Entities;
+using ContasAPagar.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContasAPagar.Infrastructure.Repositories
+{
+    public class ContaRepository : IContaARepository
+    {
+        private readonly DBConnectionContext _context;
+
+        public ContaRepository(DBConnectionContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ContaEntities> AdicionarAsync(ContaEntities conta)
+        {
+            _context.Contas.Add(conta);
+            await _context.SaveChangesAsync();
+            return conta;
+        }
+
+        public async Task<List<ContaEntities>> ListarAsync()
+        {
+            return await _context.Contas.ToListAsync();
+        }
+    }
+}
