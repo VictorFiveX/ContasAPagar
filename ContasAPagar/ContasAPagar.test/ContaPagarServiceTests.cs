9ab6a70672c8bb737c6d8796fa9483430f68188c
Victor Soares <victorsoaresds.contato@gmail.com>
2025-09-19 01:40:27 -0300

    Adicionando testes unitarios ao projeto

---
+++ ContasAPagar/ContasAPagar.test/ContaPagarServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using ContasAPagar.Application.DTOs;
+using ContasAPagar.Application.Services;
+using ContasAPagar.Domain.Entities;
+using ContasAPagar.Infrastructure.Data;
+using ContasAPagar.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+public class ContaPagarServiceTests
+{
+    [Fact]
+    public async Task Deve_Calcular_Atraso_Com_Multa_E_Juros()
+    {
+        var options = new DbContextOptionsBuilder<DBConnectionContext>()
+            .UseInMemoryDatabase("TestDb")
+            .Options;
+
+        var context = new DBConnectionContext(options);
+
+        var repository = new ContaRepository(context);
+        var service = new ContaAPagarService(repository);
+
+        var conta = new ContaDTO(
+            "Conta de Luz",
+            100,
+            DateTime.Now.AddDays(-4),
+            DateTime.Now
+        );
+
+        var result = await service.CalcularContaAsync(conta);
+
+        Assert.True(result.ValorCorrigido > 100);
+        Assert.Equal(4, result.DiasAtraso);
+    }
+}
